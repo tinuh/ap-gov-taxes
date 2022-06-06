530f58ef4f7c10f614daa7f1d30e2c3d4f27b6c1
Tinu Vanapamula <tinuvanapamula@gmail.com>
2022-06-06 09:40:10 -0400

    changes

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,71 @@
 import styles from '../styles/Home.module.css'
-import { Heading } from '@chakra-ui/react'
+import { Heading, Button, Box, Tabs, TabList, Tab, Flex, VStack } from '@chakra-ui/react'
+import Link from 'next/link';
+import { useState } from 'react';
 
 export default function Home() {
+  const tabs = {
+    "Progressive Tax": "progs",
+    "Inflation": "inf",
+    "Fiscal Policy": "",
+    "Regressive Tax": "",
+    "Proportional Tax": "",
+    "Social Welfare Policies": "",
+    "Poverty Line": ""
+  }
+  const [selected, setSelected] = useState("Progressive Tax");
+
   return (
-    <div className={styles.container}>
-      <Heading bgClip = "text" bgGradient = "linear(to-tr, white, #53c9be)" textAlign={"center"} mt = {10}>Taxes Baby</Heading>
-    </div>
+    <Box>
+      <Heading size="2xl" bgClip="text" bgGradient="linear(to-tr, white, #53c9be)" textAlign={"center"} mt={10}>Taxes Baby</Heading>
+
+      <Box textAlign="center">
+        <Link href="/survey">
+          <Button
+            size="md"
+            mt={10}
+            bgGradient="linear(to-tl,green.500,blue.500)"
+            _hover={{
+              bgGradient: "linear(to-tl,green.500,blue.500)",
+            }}
+            _active={{
+              bgGradient: "linear(to-tl,greened.500,blue.500)",
+            }}
+          >
+            Get Started
+          </Button>
+        </Link>
+      </Box>
+
+      <Flex m = {10}>
+        <Box
+          borderWidth="3px"
+          borderRadius="lg"
+          textAlign="center"
+          p = {5}
+        >
+          <Heading size = "lg" mb = {5}>Key Terms</Heading>
+          
+          <VStack>
+            {Object.keys(tabs).map(tab => (
+              <Button 
+                variant = {tab == selected ? "solid" : "outline"} 
+                onClick = {() => {setSelected(tab)}} 
+                w = "100%"
+              >
+                  {tab}
+              </Button>
+            ))}
+          </VStack>
+        </Box>
+        <Box
+          borderWidth="3px"
+          borderRadius="lg"
+        >
+          {tabs[selected]}
+        </Box>
+
+      </Flex>
+    </Box>
   )
 }
--- pages/survey.js
+++ pages/survey.js
@@ -3,16 +3,16 @@ import { useForm } from "react-hook-form";
 import { motion } from "framer-motion";
 import { slideDown, slideLeft, slideRight, stagger, fadeIn } from "../lib/animations";
 import {
-  Heading,
-  InputGroup,
-  NumberInput,
-  NumberInputField,
-  Box,
-  FormErrorMessage,
-  FormControl,
-  FormLabel,
-  InputLeftAddon,
-  Flex,
+	Heading,
+	InputGroup,
+	NumberInput,
+	NumberInputField,
+	Box,
+	FormErrorMessage,
+	FormControl,
+	FormLabel,
+	InputLeftAddon,
+	Flex,
 	Select,
 	Wrap,
 	WrapItem
@@ -22,73 +22,73 @@ import { Bar, Pie } from "react-chartjs-2";
 import "chart.js/auto";
 
 export default function Survey() {
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm();
-  const onSubmit = (data) => console.log(data);
-  const [rates, setRates] = useState([0, 0, 0, 0]);
+	const {
+		register,
+		handleSubmit,
+		watch,
+		formState: { errors },
+	} = useForm();
+	const onSubmit = (data) => console.log(data);
+	const [rates, setRates] = useState([0, 0, 0, 0]);
 
-  useEffect(() => {
+	useEffect(() => {
 		let status = watch("status");
-    let income = parseInt(watch("income")) || 0;
-    const bounds = Object.keys(brackets[status]);
-    let tax = 0;
+		let income = parseInt(watch("income")) || 0;
+		const bounds = Object.keys(brackets[status]);
+		let tax = 0;
 
-    for (let i = 0; i < bounds.length - 1; i++) {
-      if (bounds[i + 1] >= income) {
-        let taxable = income - bounds[i];
+		for (let i = 0; i < bounds.length - 1; i++) {
+			if (bounds[i + 1] >= income) {
+				let taxable = income - bounds[i];
 
-        tax += taxable * brackets[status][bounds[i]];
-        break;
-      } else {
-        tax += (bounds[i + 1] - bounds[i]) * brackets[status][bounds[i]];
-      }
-    }
-    if (income > bounds.at(-1)) {
-      tax += (income - bounds.at(-1)) * brackets[status][bounds.at(-1)];
-    }
+				tax += taxable * brackets[status][bounds[i]];
+				break;
+			} else {
+				tax += (bounds[i + 1] - bounds[i]) * brackets[status][bounds[i]];
+			}
+		}
+		if (income > bounds.at(-1)) {
+			tax += (income - bounds.at(-1)) * brackets[status][bounds.at(-1)];
+		}
 
-    let ss = income * 0.062;
-    let med = income * 0.0145;
-    let takehome = income - med - ss - tax;
+		let ss = income * 0.062;
+		let med = income * 0.0145;
+		let takehome = income - med - ss - tax;
 
-    setRates([takehome, tax, ss, med]);
-  }, [watch("income"), watch("status")]);
+		setRates([takehome, tax, ss, med]);
+	}, [watch("income"), watch("status")]);
 
-  const graphData = {
-    labels: [
-      "Take Home Pay",
-      "Federal Income Tax",
-      "Social Security",
-      "Medicare",
-    ],
-    datasets: [
-      {
-        label: "Money ($)",
-        data: rates,
-        borderWidth: 1,
-        backgroundColor: [
-          "rgba(255, 99, 132, 0.2)",
-          "rgba(54, 162, 235, 0.2)",
-          "rgba(255, 206, 86, 0.2)",
-          "rgba(75, 192, 192, 0.2)",
-          "rgba(153, 102, 255, 0.2)",
-          "rgba(255, 159, 64, 0.2)",
-        ],
-      },
-    ],
-  };
+	const graphData = {
+		labels: [
+			"Take Home Pay",
+			"Federal Income Tax",
+			"Social Security",
+			"Medicare",
+		],
+		datasets: [
+			{
+				label: "Money ($)",
+				data: rates,
+				borderWidth: 1,
+				backgroundColor: [
+					"rgba(255, 99, 132, 0.2)",
+					"rgba(54, 162, 235, 0.2)",
+					"rgba(255, 206, 86, 0.2)",
+					"rgba(75, 192, 192, 0.2)",
+					"rgba(153, 102, 255, 0.2)",
+					"rgba(255, 159, 64, 0.2)",
+				],
+			},
+		],
+	};
 
-  return (
-    <div>
+	return (
+		<div>
 			<motion.div
-				variants={stagger(0.25)}
-				initial = "initial"
-				animate = "animate"
-				exit = "exit"
+				variants={stagger(0.5)}
+				initial="initial"
+				animate="animate"
+				exit="exit"
 			>
 				<motion.div variants={slideDown}>
 					<Heading
@@ -101,66 +101,76 @@ export default function Survey() {
 					</Heading>
 				</motion.div>
 
-				<Flex flexWrap mr = {30} ml = {30} gap = {30}>
-					<Box
-						mt={30}
-						borderWidth="3px"
-						borderRadius="lg"
-						p={5}
-						flex = "1"
-					>
-						<form onSubmit={handleSubmit(onSubmit)}>
-							<FormControl mt={5} mb={5} isInvalid={errors.income}>
-								<motion.div variants={slideRight}>
-									<FormLabel>Annual Income ($)</FormLabel>
-								</motion.div>
+				<motion.div 
+					initial = {{opacity: 0}}
+					animate = {{opacity: 1}} 
+					transition = {{
+						duration: 0.5,
+						type: "easeInOut"
+					}}
+					variants={stagger(0.25)}
+				>
+					<Flex flexWrap mr={30} ml={30} gap={30}>
+						<Box
+							mt={30}
+							borderWidth="3px"
+							borderRadius="lg"
+							p={5}
+							flex="1"
+						>
+							<form onSubmit={handleSubmit(onSubmit)}>
+								<FormControl mt={5} mb={5} isInvalid={errors.income}>
+									<motion.div variants={slideRight}>
+										<FormLabel>Annual Income ($)</FormLabel>
+									</motion.div>
 
-								<motion.div variants={slideRight}>
-									<InputGroup>
-										<InputLeftAddon children="$" />
-										<NumberInput min={0} w="100%">
-											<NumberInputField
-												borderLeftRadius={0}
-												placeholder="Enter Income"
-												{...register("income", { required: true })}
-											/>
-										</NumberInput>
-									</InputGroup>
-								</motion.div>
+									<motion.div variants={slideRight}>
+										<InputGroup>
+											<InputLeftAddon children="$" />
+											<NumberInput min={0} w="100%">
+												<NumberInputField
+													borderLeftRadius={0}
+													placeholder="Enter Income"
+													{...register("income", { required: true })}
+												/>
+											</NumberInput>
+										</InputGroup>
+									</motion.div>
 
-								<FormErrorMessage>
-									{errors.income && <span>This field is required</span>}
-								</FormErrorMessage>
-							</FormControl>
+									<FormErrorMessage>
+										{errors.income && <span>This field is required</span>}
+									</FormErrorMessage>
+								</FormControl>
 
-							<FormControl>
-								<motion.div variants={slideRight}>
-									<FormLabel>Filing Status</FormLabel>
-								</motion.div>
-								<motion.div variants={slideRight}>
-									<Select {...register("status")}>
-										<option value="single">Single</option>
-										<option value="married-joint">Married Jointly</option>
-										<option value="married-separate">Married Separately</option>
-										<option value="head">Head of Household</option>
-									</Select>
-								</motion.div>
-							</FormControl>
+								<FormControl>
+									<motion.div variants={slideRight}>
+										<FormLabel>Filing Status</FormLabel>
+									</motion.div>
+									<motion.div variants={slideRight}>
+										<Select {...register("status")}>
+											<option value="single">Single</option>
+											<option value="married-joint">Married Jointly</option>
+											<option value="married-separate">Married Separately</option>
+											<option value="head">Head of Household</option>
+										</Select>
+									</motion.div>
+								</FormControl>
 
-							{/* <Button mt = {5} bgColor = "teal" type = "submit">Submit</Button> */}
-						</form>
-					</Box>
-					<Box
-						borderRadius="lg"
-						borderWidth="3px"
-						mt={30}
-						p={5}
-						flex = "1"
-					>
-						<Pie data={graphData} />
-					</Box>
-				</Flex>
+								{/* <Button mt = {5} bgColor = "teal" type = "submit">Submit</Button> */}
+							</form>
+						</Box>
+						<Box
+							borderRadius="lg"
+							borderWidth="3px"
+							mt={30}
+							p={5}
+							flex="1"
+						>
+							<Pie data={graphData} />
+						</Box>
+					</Flex>
+				</motion.div>
 			</motion.div>
-    </div>
-  );
+		</div>
+	);
 }
